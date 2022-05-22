288d200cb9b579c0a103074352e82af65c66289a
Amit Maurya <amit.maurya@oodles.io>
2022-05-22 21:04:07 +0530

    changes in stream service

--- src/main/resources/static/js/mychat.js
+++ src/main/resources/static/js/mychat.js
@@ -18,6 +18,7 @@ function connect(event) {
     Cookies.set('name', name1);
     usernamePage.classList.add('d-none');
     chatPage.classList.remove('d-none');
+    //server
     var socket = new SockJS('http://3.109.201.249:7005/sock');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, onConnected, onError);
